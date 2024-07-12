ea74394db84c772535b075325cfd44ad9f954d29
alex <sytnikov.zp@gmail.com>
2024-07-12 12:02:31 +0300

    Fix errors

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
-const chokidar = require('chokidar');  
+const chokidar = require('chokidar');
 
 const app = express();
-app.use(cors());
 const port = 5000;
 const dbPath = path.join(__dirname, 'db.json');
-console.log(`Using database file: ${dbPath}`);
 
 if (!fs.existsSync(dbPath)) {
   console.error('Database file not found:', dbPath);
@@ -19,24 +16,29 @@ if (!fs.existsSync(dbPath)) {
 
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/sytnikov.site/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/sytnikov.site/fullchain.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/sytnikov.site/fullchain.pem')
 };
+
+app.use((req, res, next) => {
+  if (req.url.startsWith('/api/')) {
+    req.url = req.url.replace('/api', '');
+  }
+  next();
+});
 
 const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults();
 
 app.use(middlewares);
-app.use('/api', router);
-
-const server = https.createServer(options, app);
-
-server.listen(port, () => {
-  console.log(`JSON Server is running on https://sytnikov.site:${port}/api`);
-});
+app.use(router);
 
 const watcher = chokidar.watch(dbPath);
-
 watcher.on('change', () => {
-  console.log('db.json has been updated.');
-  router.db = router.router(dbPath).db;
+  console.log('db.json has been updated. Reloading data...');
+  const newData = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
+  router.db.setState(newData);
+});
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`JSON Server is running on https://sytnikov.site:${port}`);
 });
