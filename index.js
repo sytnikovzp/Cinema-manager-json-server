31e74fd6597569c1fa19507164e6405c298e6006
alex <sytnikov.zp@gmail.com>
2024-07-12 08:15:30 +0300

    Create HTTPS server for Cinema manager

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const jsonServer = require('json-server');
+const path = require('path');
+
+const app = express();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/sytnikov.site/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/sytnikov.site/fullchain.pem'),
+};
+
+app.use((req, res, next) => {
+  if (req.url.startsWith('/api/')) {
+    req.url = req.url.replace('/api', '');
+  }
+  next();
+});
+
+app.use(middlewares);
+app.use('/api', router);
+
+const port = 5000;
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`JSON Server is running on https://sytnikov.site:${port}/api`);
+});
