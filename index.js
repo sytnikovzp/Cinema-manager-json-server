8a0fe9e08303db935906bb7213053d3b6c9717a6
alex <sytnikov.zp@gmail.com>
2024-08-15 13:28:51 +0300

    Refactoring server with middleware & tests

--- index.js
+++ index.js
@@ -1,62 +1,14 @@
-const express = require('express');
 const http = require('http');
-const fs = require('fs');
-const jsonServer = require('json-server');
-const path = require('path');
-const chokidar = require('chokidar');
-const cors = require('cors');
+// ======================================================
+const app = require('./src/app');
 
-const app = express();
-const port = 5000;
-const host = process.env.HOST || 'localhost';
-const dbPath = path.join(__dirname, 'db.json');
-
-if (!fs.existsSync(dbPath)) {
-  console.error('Database file not found:', dbPath);
-  process.exit(1);
-}
+// =========== Create server with HTTP module ===========
 
 const options = {};
 
-app.use(cors());
+const PORT = 5000;
+const server = http.createServer(options, app);
 
-app.use((req, res, next) => {
-  if (req.url.startsWith('/api/')) {
-    req.url = req.url.replace('/api', '');
-  }
-  next();
-});
-
-const reverseOrderMiddleware = (req, res, next) => {
-  if (!req.query._sort) {
-    req.query._sort = 'id';
-  }
-
-  if (!req.query._order) {
-    req.query._order = 'desc';
-  }
-
-  next();
-};
-
-const router = jsonServer.router(dbPath);
-const middlewares = jsonServer.defaults();
-
-app.use(middlewares);
-app.use(reverseOrderMiddleware);
-app.use(router);
-
-const watcher = chokidar.watch(dbPath);
-watcher.on('change', () => {
-  console.log('db.json has been updated. Reloading data...');
-  const newData = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
-  router.db.setState(newData);
-});
-
-app.use((req, res, next) => {
-  res.redirect('/');
-});
-
-http.createServer(options, app).listen(port, host, () => {
-  console.log(`JSON Server is running on http://${host}:${port}`);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
