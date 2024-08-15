97c222e682abc3cd559211f980e4cd5777fb8d99
alex <sytnikov.zp@gmail.com>
2024-08-15 14:12:17 +0300

    Refactoring server with middleware & tests

--- index.js
+++ index.js
@@ -1,64 +1,17 @@
-const express = require('express');
 const https = require('https');
-const fs = require('fs');
-const jsonServer = require('json-server');
-const path = require('path');
-const chokidar = require('chokidar');
-const cors = require('cors');
+// ======================================================
+const app = require('./src/app');
 
-const app = express();
-const port = 5000;
-const dbPath = path.join(__dirname, 'db.json');
-
-if (!fs.existsSync(dbPath)) {
-  console.error('Database file not found:', dbPath);
-  process.exit(1);
-}
+// =========== Create server with HTTPS module ===========
 
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/sytnikov.site/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/sytnikov.site/fullchain.pem'),
 };
 
-app.use(cors());
+const PORT = 5000;
+const server = https.createServer(options, app);
 
-app.use((req, res, next) => {
-  if (req.url.startsWith('/api/')) {
-    req.url = req.url.replace('/api', '');
-  }
-  next();
-});
-
-const reverseOrderMiddleware = (req, res, next) => {
-  if (!req.query._sort) {
-    req.query._sort = 'id';
-  }
-
-  if (!req.query._order) {
-    req.query._order = 'desc';
-  }
-
-  next();
-};
-
-const router = jsonServer.router(dbPath);
-const middlewares = jsonServer.defaults();
-
-app.use(middlewares);
-app.use(reverseOrderMiddleware);
-app.use(router);
-
-const watcher = chokidar.watch(dbPath);
-watcher.on('change', () => {
-  console.log('db.json has been updated. Reloading data...');
-  const newData = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
-  router.db.setState(newData);
-});
-
-app.use((req, res, next) => {
-  res.redirect('/');
-});
-
-https.createServer(options, app).listen(port, () => {
-  console.log(`JSON Server is running on https://sytnikov.site:${port}`);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on https://sytnikov.site:${PORT}`);
 });
